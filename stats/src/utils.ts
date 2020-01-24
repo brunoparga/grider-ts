53c078e13b947ae8590641e26747d07ae6a4444a
Bruno Parga <brunoparga@gmail.com>
2020-01-24 11:45:03 -0300

    Create utility function to parse a date string

---
+++ stats/src/utils.ts
@@ -0,0 +1,9 @@
+export const parseDate = (dateString: string): Date => {
+  // 28/10/2018
+  const dateParts =
+    dateString
+      .split('/')
+      .map((value: string): number => parseInt(value));
+
+  return new Date(dateParts[2], dateParts[1] - 1, dateParts[0])
+}
