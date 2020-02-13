985a1fc945b596358958b662e6d5d9826f891855
Bruno Parga <brunoparga@gmail.com>
2020-02-13 20:04:04 -0300

    Implement view nesting

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,11 +1,11 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const root = document.getElementById('root');
 
 if (root) {
-  const userForm = new UserForm(root, User.build({ name: 'NAME', age: 20 }));
-  userForm.render();
+  const userEdit = new UserEdit(root, User.build({ name: 'NAME', age: 20 }));
+  userEdit.render();
 } else {
   throw new Error('Root element not found');
 }
--- web/src/views/View.ts
+++ web/src/views/View.ts
@@ -1,6 +1,8 @@
 import { Model } from '../models/Model';
 
 export abstract class View<T extends Model<U>, U> {
+  regions: { [key: string]: HTMLElement } = {};
+
   constructor(
     public parent: HTMLElement,
     public model: T,
@@ -8,9 +10,15 @@ export abstract class View<T extends Model<U>, U> {
     this.bindModel();
   }
 
-  abstract eventsMap(): { [key: string]: () => void };
-
   abstract template(): string;
+
+  eventsMap(): { [key: string]: () => void } {
+    return {};
+  }
+
+  regionsMap(): { [key: string]: string } {
+    return {};
+  }
 
   bindModel = (): void => {
     this.model.on('change', () => this.render());
@@ -26,11 +34,26 @@ export abstract class View<T extends Model<U>, U> {
       });
   }
 
+  mapRegions(fragment: DocumentFragment): void {
+    Object.entries(this.regionsMap())
+      .forEach(([key, value]) => {
+        const element = fragment.querySelector(value) as HTMLElement;
+        if (element) {
+          this.regions[key] = element;
+        }
+      });
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  onRender(): void { }
+
   render(): void {
     this.parent.innerHTML = '';
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
     this.bindEvents(templateElement.content);
+    this.mapRegions(templateElement.content);
+    this.onRender();
     this.parent.append(templateElement.content);
   }
 }
