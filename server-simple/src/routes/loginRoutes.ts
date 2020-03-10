f4206fc904340c273916138750401f26ddeb3b31
Bruno Parga <brunoparga@gmail.com>
2020-03-10 17:37:08 -0300

    Implement basic login GET and POST routes

--- server-simple/src/routes/loginRoutes.ts
+++ server-simple/src/routes/loginRoutes.ts
@@ -2,12 +2,27 @@ import { Request, Response, Router } from 'express';
 
 const router = Router();
 
-router.get('/', (req: Request, res: Response) => {
+router.get('/login', (req: Request, res: Response) => {
   res.send(`
-    <div>
-      <h1>Hello there!</h1>
-    </div>
+    <form method="POST">
+      <div>
+        <label>Email
+          <input name="email" />
+        </label>
+      </div>
+      <div>
+        <label>Password
+          <input name="password" type="password" />
+        </label>
+      </div>
+      <button>Submit</button>
+    </form>
   `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send(email + password);
 });
 
 export default router;
